69d79234f31a9e87c3922f63aa530d4a0b975610
Semen Boldyrev <233458958+SemenBoldyrev@users.noreply.github.com>
2026-05-16 17:20:16 +0300

    changes, changes

--- Backend.js
+++ Backend.js
@@ -99,7 +99,7 @@ function SendRequest(sql, res)
             res.json(result);
         });
     } catch (error) {
-        console.log(`Error occurred while sending request: ${error}\nSQL: ${sql}`);
+        console.log(`Error occurred while sending request: ${sql}`);
         res.status(500).json({ error: 'Internal Server Error' });
         throw error;
     }
